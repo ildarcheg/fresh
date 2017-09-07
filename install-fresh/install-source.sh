b155014ccd5614f8bdcdc8ad1fe4567d940f9541
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-09-07 12:24:15 +0700

    -y command added

--- install-fresh/install-source.sh
+++ install-fresh/install-source.sh
@@ -1,8 +1,8 @@
-sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get update -y
+sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:git-core/ppa
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install git-lfs
+sudo apt-get install -y git-lfs
 git lfs install
 cd
 git clone https://github.com/ildarcheg/fresh-install.git
